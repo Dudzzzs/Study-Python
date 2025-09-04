463dbdbef41f7c1ea1954efdc3f262813c16646e
Eduardo Lucas <dudlucas123@gmail.com>
2025-09-03 21:32:05 -0300

    Aprimorando em condicionais

--- desafio41-50.py
+++ desafio41-50.py
@@ -61,3 +61,72 @@ if (r1 + r2) > r3 and (r1 + r3) > r2 and (r2 + r3) > r1:
     print('Com essas 3 retas de comprimento {}, {} e {} você pode formar um triângulo {}!'.format(r1, r2, r3, triangulo))
 else:
     print('Com essas 3 retas de comprimento {}, {} e {} você não pode formar um triângulo!'.format(r1, r2, r3))
+
+# ===== Desafio 45 =====
+
+peso = float(input('Digite o seu peso atual: '))
+altura = float(input('Digite a sua altura atual: '))
+
+imc = peso / (altura ** 2)
+
+if imc < 18.5:
+    print('Seu imc é de {:.2f}kg/m2 e por isso você está ABAIXO DO PESO.'.format(imc))
+elif imc >= 18.5 and imc < 25:
+    print('Seu imc é de {:.2f}kg/m2 e por isso você está no PESO IDEAL.'.format(imc))
+elif imc >= 25 and imc < 30:
+    print('Seu imc é de {:.2f}kg/m2 e por isso você está SOBREPESO.'.format(imc))
+elif imc >= 30 and imc < 40:
+    print('Seu imc é de {:.2f}kg/m2 e por isso você está na OBESIDADE.'.format(imc))
+else:
+    print('Seu imc é de {:.2f}kg/m2 e por isso você está na OBESIDADE MÓRBIDA.'.format(imc))
+
+# ===== Desafio 46 =====
+
+import random
+
+preco = random.randint(1,100)
+
+print('O preço do produto é \033[1;31mR${}\033[m'.format(preco))
+
+print('Qual será a forma de pagamento? \n1 - A vista no dinheiro ou cheque; \n2 - A vista no cartão; \n3 - Cartão de 2x; \n4 - Cartão de 3x ou mais.' )
+pagamento = int(input('Escolha a forma de apagamento: '))
+
+if pagamento == 1:
+    preco = preco - (preco * (10 / 100))
+    print('Para pagamento a vista em dinheiro ou cheque temos 10% de desconto e com isso o valor do produto será R${:.2f}'.format(preco))
+elif pagamento == 2:
+    preco = preco - (preco * (5 / 100))
+    print('Para pagamento a vista no cartão temos 5% de desconto e com isso o valor do produto será R${:.2f}'.format(preco))
+elif pagamento == 3:
+    print('Para pagamento no cartão até 2x não temos desconto e com isso o valor do produto será R${:.2f}'.format(preco))
+elif pagamento == 4:
+    preco = preco + (preco * (20 / 100))
+    print('Para pagamento no cartão de 3x ou mais temos 20% de juros e com isso o valor do produto será R${:.2f}'.format(preco))
+else:
+    print('Opção inválida!')
+
+# ===== Desafio 47 =====
+
+import random
+
+print('\033[1;33mVamos jogar JOKENPÔ!\033[m')
+
+computadorJogada = random.choice(['pedra', 'papel', 'tesoura'])
+minhaJogada = input('Você quer pedra, papel ou tesoura? ')
+
+if computadorJogada == 'tesoura' and minhaJogada == 'papel':
+    print('O computador jogou {} e você {}. COMPUTADOR VENCE!'.format(computadorJogada, minhaJogada))
+elif computadorJogada == 'tesoura' and minhaJogada == 'pedra':
+    print('O computador jogou {} e você {}. VOCÊ VENCE!'.format(computadorJogada, minhaJogada))
+elif computadorJogada == 'papel' and minhaJogada == 'pedra':
+    print('O computador jogou {} e você {}. COMPUTADOR VENCE!'.format(computadorJogada, minhaJogada))
+elif computadorJogada == 'papel' and minhaJogada == 'tesoura':
+    print('O computador jogou {} e você {}. VOCÊ VENCE!'.format(computadorJogada, minhaJogada))
+elif computadorJogada == 'pedra' and minhaJogada == 'papel':
+    print('O computador jogou {} e você {}. VOCÊ VENCE!'.format(computadorJogada, minhaJogada))
+elif computadorJogada == 'pedra' and minhaJogada == 'tesoura':
+    print('O computador jogou {} e você {}. COMPUTADOR VENCE!'.format(computadorJogada, minhaJogada))
+elif computadorJogada == minhaJogada:
+    print('O computador jogou {} e você {}. EMPATE!'.format(computadorJogada, minhaJogada))
+else:
+    print('Você não sabe jogar!')
--- rascunho.py
+++ rascunho.py
@@ -1,19 +1,3 @@
-# ===== Desafio 44 =====
-
-r1 = float(input('Digite o comprimento da primeira reta: '))
-r2 = float(input('Digite o comprimento da segunda reta: '))
-r3 = float(input('Digite o comprimento da terceira reta: '))
-triangulo = str
-
-if r1 == r2 and r2 == r3:
-    triangulo = 'equilátero'
-elif r1 == r2 or r2 == r3 or r1 == r3:
-    triangulo = 'isósceles'
-elif r1 != r2 and r1 != r3 and r2 != r3:
-    triangulo = 'escaleno'
+# ===== Desafio 48 =====
 
 
-if (r1 + r2) > r3 and (r1 + r3) > r2 and (r2 + r3) > r1:
-    print('Com essas 3 retas de comprimento {}, {} e {} você pode formar um triângulo {}!'.format(r1, r2, r3, triangulo))
-else:
-    print('Com essas 3 retas de comprimento {}, {} e {} você não pode formar um triângulo!'.format(r1, r2, r3))
