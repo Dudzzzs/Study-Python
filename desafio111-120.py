9d6a304fea05cbc4629b483883f2e94e13f0ef17
Eduardo Lucas <dudlucas123@gmail.com>
2025-10-13 21:22:27 -0300

    Atualizando exercício

--- desafio111-120.py
+++ desafio111-120.py
@@ -71,3 +71,24 @@ except urllib.error.URLError:
     print('\033[1;31mO SITE NÃO ESTÁ ACESSÍVEL\033[m')
 else:
     print('\033[1;32mO SITE ESTÁ ACESSSÍVEL\033[m')
+
+# ===== Desafio 117 =====
+
+from import_modulos.pessoas import pessoas
+
+while True:
+    msg = '\033[1;33mMENU PRINCIPAL \033[m'
+    tam = len(msg) + 4
+    print('=' * tam)
+    print(f'       {msg}')
+    print()
+    print('\033[1;35m1: Cadastrar pessoa; \n2: Exibir pessoas; \n3: Sair do programa.\033[m')
+    print('=' * tam)
+
+    opcao = int(input('\033[1;35mOpção: \033[m'))
+
+    if opcao == 3:
+        print('\033[1;34mPrograma encerrado!\033[m')
+        break
+    else:
+        pessoas.banco_dados(opcao)
